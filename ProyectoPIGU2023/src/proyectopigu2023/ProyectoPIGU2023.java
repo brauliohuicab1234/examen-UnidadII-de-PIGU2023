ed802e25922e3b704ce3220d9178ecfa2ee02b29
brauliohuicab1234 <129221141+brauliohuicab1234@users.noreply.github.com>
2023-05-23 00:44:58 -0600

    commit 1
    metodo con mi nombre

---
+++ ProyectoPIGU2023/src/proyectopigu2023/ProyectoPIGU2023.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectopigu2023;
+
+public class ProyectoPIGU2023 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(Braulio63913());
+    }
+    
+    public static String Braulio63913(){
+        String nombre = "Braulio Rafael ";
+        String apellido = "Huicab May";
+        String Carrera = "Ingenieria en Sistemas Computacionales";
+        return "Soy el Alumno "+nombre +apellido +" y estudio la carrera: "+Carrera ;
+    }
+    
+}
